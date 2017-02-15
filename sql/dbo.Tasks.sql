bb92b5f766f7555ecdef4df18ce1dfb3e7f69d54
dmremenec <remenets@glonasssoft.ru>
2017-02-15 14:06:12 +0300

    sql notes

--- sql/dbo.Tasks.sql
+++ sql/dbo.Tasks.sql
@@ -1,20 +1,10 @@
-USE [taskerDB]
-GO
-
-/****** Объект: Table [dbo].[Tasks] Дата скрипта: 13.02.2017 19:51:26 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[Tasks] (
-    [Id]            INT           NOT NULL,
+    [Id]            INT           NOT NULL,       --
     [Name]          VARCHAR (50)  NOT NULL,
     [ExpectedStart] DATETIME      NOT NULL,
-    [Params]        VARCHAR (500) NULL,
-    [Status]        tinyint NOT NULL DEFAULT 0,
-    [Type]          VARCHAR (50)  NOT NULL
+    [Params]        VARCHAR (500) NULL,           -- параметры
+    [Status]        tinyint NOT NULL DEFAULT 0,   -- состояние: Новая(0), Запланирована(1), Выполнена успещно(2), С ошибкой(3)  
+    [Type]          VARCHAR (50)  NOT NULL        -- тип задания: File, Email
 );
 
 
